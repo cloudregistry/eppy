3c97cd4a22a29fe4ef78e4d154ba2b4d2834a192
Wil Tan <wil@cloudregistry.net>
2010-08-17 00:07:17 +1000

    distutils setup.py

---
+++ src/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+from eppy import __version__
+
+
+setup(
+    name = "eppy",
+    version = __version__,
+    author = "Wil Tan",
+    author_email = "wil@cloudregistry.net",
+    description = "EPP Client for Python",
+    license = "MIT/X",
+    install_requires = [],
+    packages = find_packages(),
+)
